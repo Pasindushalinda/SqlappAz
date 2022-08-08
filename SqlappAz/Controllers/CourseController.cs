47ea5cf5ebcdc4a1c8494315ed14117f2ece3213
Pasindushalinda <pshalinda5@gmail.com>
2022-08-08 13:01:35 +0530

    Add project files.

---
+++ SqlappAz/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SqlApp.Models;
+using SqlApp.Services;
+using System.Collections.Generic;
+
+namespace SqlApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public CourseController(CourseService _svc, IConfiguration configuration)
+        {
+            _course_service = _svc;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            // Use the configuration class to get the connection string
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
